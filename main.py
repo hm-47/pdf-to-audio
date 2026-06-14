9ecdf59af8592ea061681927a0815c69a07616b2
HM <himanshmewada47@gmail.com>
2026-06-14 12:30:34 +0530

    Project Completed

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter.filedialog import askopenfilename
+
+import PyPDF2
+import pyttsx3
+
+book = askopenfilename()
+pdfreader = PyPDF2.PdfFileReader(book)
+pages = pdfreader.numPages
+
+for num in range(0, pages):
+    page = pdfreader.getPage(num)
+    text = page.extractText()
+    player = pyttsx3.init()
+    player.say(text)
+    player.runAndWait()
